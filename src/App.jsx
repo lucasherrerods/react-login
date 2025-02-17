5815ae7619278eb4a599853dde65a9b49d9d998b
lucasherrerods <lucasherrerosilva.lh@gmail.com>
2025-02-17 20:23:23 -0300

    feat: add Login and List pages with routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router'
 import Register from './pages/Register'
+import Login from './pages/Login'
+import List from './pages/List'
 
 function App() {
 
@@ -7,6 +9,8 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Register />}></Route>
+        <Route path='/login' element={<Login />}></Route>
+        <Route path='/list' element={<List />}></Route>
       </Routes>
     </BrowserRouter>
   )
