b48c9241bfc3cd187421ee609f9fd96c5f92a8ee
lucasherrerods <lucasherrerosilva.lh@gmail.com>
2025-02-17 20:45:16 -0300

    feat: include a simple header

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router'
 import Register from './pages/Register'
 import Login from './pages/Login'
 import List from './pages/List'
+import Header from './components/Header'
 
 function App() {
 
   return (
     <BrowserRouter>
+      <Header></Header>
       <Routes>
         <Route path='/' element={<Register />}></Route>
         <Route path='/login' element={<Login />}></Route>
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <div className='w-full h-15 bg-blue-500 flex items-center justify-center'>
+      <h1 className='text-xl text-white font-bold'>Sistema de Cadastro de Usuários</h1>
+    </div>
+  )
+}
+
+export default Header
