ff2ee82713890f8fdf72041a187770510a4db92e
lucasherrerods <lucasherrerosilva.lh@gmail.com>
2025-02-20 01:54:53 -0300

    feat: show active users and apply styles

--- src/pages/List/index.jsx
+++ src/pages/List/index.jsx
@@ -24,7 +24,22 @@ function List() {
 
   return (
     <div>
-      <h2>Listar Usuários</h2>
+      <div className='m-12 flex items-center justify-between'>
+        <h2 className='text-2xl font-bold'>Usuários ativos</h2>
+        <div>
+          <p>Database: MongoDB</p>
+          <p>Admin: lucasherrerods</p>
+        </div>
+      </div>
+      <ul className='w-full md:w-auto p-8 m-10 grid grid-cols-3 gap-20 bg-inherit border border-gray-300 shadow-lg rounded-md'>
+        {allUsers && allUsers.length > 0 && allUsers.map((user) => (
+          <li key={user.id} className='py-4 px-8 rounded-lg bg-blue-200 text-white hover:scale-105 duration-300 ease-in-out hover:bg-indigo-900'>
+            <p className='tracking-[.1rem]'><span className='font-bold'>ID:</span> {user.id}</p>
+            <p className='tracking-[.1rem]'><span className='font-bold'>Nome:</span> {user.name}</p>
+            <p className='tracking-[.1rem]'><span className='font-bold tracking-normal'>E-mail:</span> {user.email}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
